d9252a056a86ae4dc19856c5c9b8bf5357e6ea55
discontinuedtv <daniel@discontinued.tv>
2020-10-12 23:35:25 +0200

    added additional views and styling

--- BotbrownWPF/MainWindow.xaml.cs
+++ BotbrownWPF/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace BotbrownWPF
+﻿namespace BotbrownWPF
 {
+    using BotbrownWPF.ViewModels;
+    using System.Windows;
+
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
@@ -23,6 +11,10 @@ namespace BotbrownWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new MainViewModel();
+
+            this.MenuTabControl.SelectedIndex = 1;
         }
     }
 }
---
+++ BotbrownWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using BotbrownWPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BotbrownWPF.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<TabItem> Tabs
+        {
+            get
+            {
+                List<TabItem> tabs = new List<TabItem>();
+                tabs.Add(new TabItem
+                {
+                    VerticalContentAlignment = VerticalAlignment.Top,
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    IsEnabled = false,
+                    Name = "Logo",
+                    IsSelected = false,
+                    Height = 112,
+                    Header = new Image
+                    {
+                        Width = 112,
+                        Source = new BitmapImage(new Uri("pack://application:,,,/Assets/doc-112x112.png"))
+                    }
+                });
+
+                tabs.Add(new TabItem
+                {
+                    Header = "Startseite",
+                    Content = new Startpage()
+                });
+
+                tabs.Add(new TabItem
+                {
+                    Header = "Kommandos",
+                    Content = new Commandspage()
+                });
+
+                tabs.Add(new TabItem
+                {
+                    Header = "Sounds",
+                    Content = new Soundspage()
+                });
+
+                tabs.Add(new TabItem
+                {
+                    Header = "Eigenschaften",
+                    Content = new Settingspage()
+                });
+
+                return tabs;
+            }
+        }
+    }
+}
